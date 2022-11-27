5c6b0b74de69147dec118595a3f3fa9e77fee40f
Elli610 <113121626+Elli610@users.noreply.github.com>
2022-11-27 12:49:57 +0100

    working on db connection and data in urls

--- Direct messages/dm.php
+++ Direct messages/dm.php
@@ -1,71 +1,121 @@
 <!DOCTYPE html>
-
-<html> 
-    
-    <head>
-        <meta charset="utf-8">
-        <title> Mes DM </title>
-    </head>
-    <body>
-        <h1> Mes DM </h1>
-        <div className="mes_conversations">
-          <h2>Mes conversations</h2>
-            <div classname="messages">
-            <?php
-					foreach($chat_data as $chat)
-					{
-						if(isset($_SESSION['user_data'][$chat['userid']]))
-						{
-							$from = 'Me';
-							$row_class = 'row justify-content-start';
-							$background_class = 'text-dark alert-light';
-						}
-						else
-						{
-							$from = $chat['user_name'];
-							$row_class = 'row justify-content-end';
-							$background_class = 'alert-success';
-						}
-
-						echo '
-						<div class="'.$row_class.'">
-							<div class="col-sm-10">
-								<div class="shadow-sm alert '.$background_class.'">
-									<b>'.$from.' - </b>'.$chat["msg"].'
-									<br />
-									<div class="text-right">
-										<small><i>'.$chat["created_on"].'</i></small>
-									</div>
-								</div>
-							</div>
-						</div>
-						';
-					}
-					?>
-					</div>
-            </div>
-          <div className="nouveau_msg">
-                <div className="creer_msg">
-                    <form id="monForm" action="" method="post">
-                        <textarea className="text_area_new_msg" placeholder="Ecrivez votre message"></textarea>
-                        <button type="submit">Envoyer</button>
-                    </form>
-                </div>    
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mes DM</title>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="title">Mes DM</div>
+        <div class="form">
+            <div class="bot-inbox inbox">
+                <div class="icon">
+                    <i class="fas fa-user"></i>
+                </div>
+                <div class="msg-header">
+                    <p>Dites ce qui vous passe par la tête ! </p>
+                </div>
             </div>
         </div>
-        
-        <p> <a href="index.php"> Retour à l'accueil </a> </p>
-    </body>
-
+        <div class="typing-field">
+			<form>
+            <div class="input-data">
+                <input id="data" type="text" name = "newMessage" placeholder="Ecrivez quelque chose .." required>
+                <button type = "submit" id="send-btn">Envoyer</button>
+            </div>
+			</form>
+        </div>
+    </div>
+    <script>
+        $(document).ready(function(){
+            $("#send-btn").on("click", function(){
+                $value = $("#data").val();
+                $msg = '<div class="user-inbox inbox"><div class="msg-header"><p>'+ $value +'</p></div></div>';
+                $(".form").append($msg);
+                $("#data").val('');
+                
+                // ajax code
+                $.ajax({
+                    url: 'message.php',
+                    type: 'POST',
+                    data: 'text='+$value,
+                    success: function(result){
+                        $replay = '<div class="bot-inbox inbox"><div class="icon"><i class="fas fa-user"></i></div><div class="msg-header"><p>'+ result +'</p></div></div>';
+                        $(".form").append($replay);
+                        $(".form").scrollTop($(".form")[0].scrollHeight);
+                    }
+                });
+            });
+        });
+    </script>
+    
+</body>
 </html>
 
 <?php
-curent_user = $_GET['user'] // id du user courant
-conv_id = $_GET['conv_id'] // id de la conversation courante
 
-// récupération des messages de la convo
-$query = "SELECT 'sender_id', 'text', 'date' FROM `messages` WHERE 'convo_id'='conv_id';
-VALUES ('$sender_id', '$text', '$date')";
+// check if user id is in url
+if(isset($_GET['id']))
+{
+	// get user id
+	$user_id = $_GET['id'];
+}
+else
+{
+	header('location:menu.php');
+}
+
+// check if convId is in url
+if(isset($_GET['convId']))
+{
+	// get convId
+	$convId = $_GET['convId'];
+}
+else
+{
+	header('location:menu.php');
+}
+
+//SQL
+
+// connect to postgresql
+$user = "grp47oxh6hjegww"; 
+$password = "99yXmThpFno";
+$host = "https://pga.esilv.olfsoftware.fr/";
+$port = "5432";
+$dbname = "pggrp4";
+//$myPDO = new PDO("pgsql:host=$host;dbname=$dbname', $user, $password");
+$connect = pg_connect("host=$host dbname=$dbname user=$user password=$password");
+
+if(!$connect){
+	echo "Unable to connect to the database.";
+} 
+else{
+	
+	$result = pg_query($connect, "SELECT sender_id,text  FROM messages WHERE convo_id = $convId");
+
+	while ($row = pg_fetch_row($result)) {
+		echo "Sender ID: $row[0]  Message: $row[1]";
+		echo "<br />\n";
+}
+
+}
+
+// Add new message to the database
+
+// get message
+$message = $_POST['newMessage'];
+$user_file = fopen('user_file.txt', 'a+');
+fputs($user_file, "======================================== \n");
+fputs($user_file, $message . "\n");
+	
+// insert message into database
+$sql = "INSERT INTO messages (sender_id, convo_id, text) VALUES ('$user_id', '$convId', '$message')";
+$result = pg_query($connect, $sql);
+	
+
 
 
 ?>
---
+++ Direct messages/menu.php
@@ -0,0 +1,15 @@
+
+
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title> Menu </title>
+    </head>
+    <body>
+        <p> Menu </p>
+    </body>
+</html
+<?php
+?>
