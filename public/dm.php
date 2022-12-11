6e3ed72684a109d7053ff1e72c7ab2f7a41f1ab0
Elli610 <113121626+Elli610@users.noreply.github.com>
2022-12-11 18:24:28 +0100

    update

---
+++ public/dm.php
@@ -0,0 +1,138 @@
+ 
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8" />
+        <link href="./css/stylecss.css" rel = "stylesheet" type = "text/css">
+        <title>chat DM</title>
+        <meta name="description" content="chatbox" />
+    </head>
+    <body>
+    <?php
+    // connect to postgresql
+    $user = "grp47oxh6hjegww"; 
+    $password = "99yXmThpFno";
+    $host = "esilv.olfsoftware.fr";
+    $port = "5432";
+    $dbname = "pggrp4";
+
+    $connect = pg_connect("host=$host port=5432 dbname=$dbname user=$user password=$password");
+
+    if(!$connect){
+	    echo "Unable to connect to the database.";
+    } 
+
+    if(!isset($_GET['id'])) // vérifie si id est dans l'url du client est dans l'url
+    {
+        header('location:https://grp4.esilv.olfsoftware.fr/app.php');
+    }
+    else {
+        $user_id = $_GET['id']; 
+        if(isset($_GET['convId'])){ // check si conversation id est dans l'url
+	        $convId = $_GET['convId'];
+        }
+        else{
+	        header('location:dm_menu.php?id='.$user_id);
+        }
+
+
+    ?>
+        <div id="wrapper">
+            <div id="menu">
+                <p class="welcome">Heureux de vous revoir, <b><?php 
+                    $query = "SELECT username FROM webdev.users WHERE id = '".$user_id."'";
+                    $username = pg_query($connect, $query);
+                    $username = pg_fetch_row($username);
+                    $username = $username[0];
+                echo $username; 
+                ?></b> !!!</p>
+            </div>
+            <div id="retourMenu">
+                <p><a id="retour_dm_menu" href="dm_menu.php">Revenir au menu</a></p>
+            </div>
+ 
+            <div id="chatbox">
+            <?php
+            
+            // récupération des anciens messages si ils existent dans $result
+            
+            $query = "SELECT * FROM webdev.messages WHERE convo_id = '".$convId."'";
+            //$query = "SELECT * FROM users WHERE email = '".$_POST['email']."' AND password = '$password_md5'";
+
+            $oldMessages = pg_query($connect, $query); 
+
+            //$sender="";
+            echo "<div id=text_conv_area>";
+	        while ($row = pg_fetch_row($oldMessages)) {
+                $date = $row[5];
+                if($row[2] == $user_id){
+                    $sender = "Vous";
+                    echo "<p id='vous'>$date |  <strong>$sender</strong> :  $row[3]</p>";
+                }
+                else{
+                    $query = "SELECT user_0, user_1 FROM webdev.conversations WHERE id = '".$convId."'";
+                    $people = pg_query($connect, $query);
+                    $people = pg_fetch_row($people);
+                    if($people[0] == $user_id){
+                        $sender = $people[1];
+                    }
+                    else{
+                        $sender = $people[0];
+                    }
+                    $query = "SELECT username FROM webdev.users WHERE id = '".$sender."'";
+                    $sender = pg_query($connect, $query);
+                    $sender = pg_fetch_row($sender);
+                    $sender = $sender[0];
+
+                    echo "<p id='lui'>$date |  <strong>$sender</strong> :  $row[3]</p>";
+                }
+		        
+            }
+            echo "</div>";
+            
+            ?>
+            
+            </div>
+ 
+            <form method="post">
+                <input name="usermsg" type="text" id="usermsg" value=""/>
+                <p><input type="submit" value="Envoyer"></p>
+            </form>
+        </div>
+    </body>
+
+    <?php
+    }
+    // add message to db
+    if(isset($_POST['usermsg'])){
+        $text = $_POST['usermsg'];
+        $text_message = "<div class='msgln'><span class='chat-time'>".date("g:i A")."</span> <b class='user-name'>".'pseudo'."</b> ".stripslashes(htmlspecialchars($text))."<br></div>";
+        // connect to postgres
+        $user = "grp47oxh6hjegww"; 
+        $password = "99yXmThpFno";
+        $host = "esilv.olfsoftware.fr";
+        $port = "5432";
+        $dbname = "pggrp4";
+        $connect = pg_connect("host=$host port=5432 dbname=$dbname user=$user password=$password");
+        if(!$connect){
+            echo "Unable to connect to the database.";
+        }
+        $id = $_GET['id'];
+        
+        $time = time();
+        $date = date("Y-m-d H:i:s", $time);
+    
+
+        $query = "INSERT INTO webdev.messages VALUES ('$time', '$convId', '$id', '$text', null, '$date')";
+        $newMessages = pg_query($connect, $query);
+        if (!$newMessages) {
+            echo "Une erreur s'est produite.\n";
+            exit;
+        }
+        else{
+            header('location:dm.php?id='.$id.'&convId='.$convId);
+        }
+    }
+
+    ?>
+</html>
---
+++ public/dm_menu.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html >
+
+<html lang="fr"> 
+    
+    <head>
+        <meta charset="utf-8">
+        <link href="./css/stylecss.css" rel = "stylesheet" type = "text/css">
+        <title> Mes DM </title>
+    </head>
+    <header>
+
+	<div class="container">
+
+		<div class="profile">
+
+			<div class="profile-image">
+
+			<div style="cursor:pointer;" style class="image-upload">
+		     	<label style="cursor:pointer;" for="file-input">
+				   <img class="picture" id="picture" style="cursor:pointer;"onClick="onFileSelected()" src="https://images.unsplash.com/photo-1513721032312-6a18a42c8763?w=152&h=152&fit=crop&crop=faces" alt="">
+				</label>
+				<input style="display:none;" id="file-input" type="file"  accept="image/*" onchange="document.getElementById('picture').src = window.URL.createObjectURL(this.files[0])"/>
+            </div>
+
+			</div>
+
+			<div class="profile-user-settings">
+
+				<h1 class="profile-user-name"><?php 
+                    $user_id = $_GET['id'];
+                    $user = "grp47oxh6hjegww"; 
+                    $password = "99yXmThpFno";
+                    $host = "esilv.olfsoftware.fr";
+                    $port = "5432";
+                    $dbname = "pggrp4";
+                
+                    $connect = pg_connect("host=$host port=5432 dbname=$dbname user=$user password=$password");
+                    $query = "SELECT username FROM webdev.users WHERE id = '".$user_id."'";
+                    $username = pg_query($connect, $query);
+                    $username = pg_fetch_row($username);
+                    $username = $username[0];
+                echo $username;
+                ?></h1>
+
+			</div>
+		</div>
+		<!-- End of profile section -->
+
+	</div>
+	<!-- End of container -->
+
+</header>
+    <body>
+        <h1 id=titre> Mes DM </h1>
+        <div className="mes_conversations">
+          <h2 id=mes_convs>Mes conversations</h2>
+            <ul>
+                <?php
+                    if(!isset($_GET['id'])) // vérifie si id est dans l'url du client est dans l'url
+                    {
+                        header('location:https://grp4.esilv.olfsoftware.fr/');
+                    }
+                    // connect to postgresql
+                    $user = "grp47oxh6hjegww"; 
+                    $password = "99yXmThpFno";
+                    $host = "esilv.olfsoftware.fr";
+                    $port = "5432";
+                    $dbname = "pggrp4";
+
+                    $connect = pg_connect("host=$host port=5432 dbname=$dbname user=$user password=$password");
+
+                    if(!$connect){
+                        echo "Unable to connect to the database.";
+                    } 
+                    $user_id = $_GET['id'];
+
+                    // Récupération des conversations de l'utilisateur
+                    $query = "SELECT id,user_0,user_1 FROM webdev.conversations WHERE user_0 = '".$user_id."' OR user_1 = '".$user_id."'";
+                    $conv = pg_query($connect, $query);
+
+                    // affichage des conversations
+                    if(pg_num_rows($conv) != 0){
+                        echo "<div id=conv_area>";
+                        while($row = pg_fetch_row($conv)){
+                            if($row[1] == $user_id){
+                                $vousId = $row[1];
+                                $pasVousId = $row[2];
+                            }
+                            else{
+                                if($row[2] == $user_id){
+                                    $vousId = $row[2];
+                                    $pasVousId = $row[1];
+                                }
+                                else{
+                                    echo "Erreur";
+                                    $vousId = "Erreur";
+                                }
+                            }
+
+                            // récupère le usernamer de l'autre utilisateur
+                            $query = "SELECT username FROM webdev.users WHERE id = '".$pasVousId."'";
+                            $username = pg_query($connect, $query);
+                            $username = pg_fetch_row($username);
+
+                            echo "<li><a id=link href='dm.php?id=".$user_id."&convId=".$row[0]."'>".$username[0]."</a></li>";
+                        }
+                        echo"</div>";
+                          
+                    }
+                    else{
+                        echo "Vous n'avez pas de conversations";
+                    }                   
+                      
+                ?>
+            </ul>
+        </div>
+        
+    </body>
+
+
+<?php
+
+    if(!isset($_GET['id'])) // vérifie si id est dans l'url
+    {
+        header('location:https://grp4.esilv.olfsoftware.fr/');
+    }
+    else {
+        $user_id = $_GET['id'];
+    }
+
+?>
+
+</html>
